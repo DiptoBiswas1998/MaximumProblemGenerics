f021e425907b7d6380e03cd7648f1452f8468d7a
unknown <diptobiswas1998@gmail.com>
2020-10-28 07:09:49 +0530

    [Dipto] refactor: Adding the welcome msg

---
+++ MaximumProblemGenerics/MaximumProblemGenerics/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MaximumProblemGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Maximum Problem using Generics!");
+        }
+    }
+}
